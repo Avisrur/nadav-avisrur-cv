bb742fc85d461c8d881e596002ce03bd2d0c5177
Avisrur <nadavavisrur@gmail.com>
2020-04-24 18:52:07 +0300

    Updates

---
+++ precache-manifest.01614a19abb7e011285262a76b7f8126.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4533957c4da25bfa64b58d9fbc580a7b",
+    "url": "/nadav-avisrur-cv/index.html"
+  },
+  {
+    "revision": "19955586454447dd0cad",
+    "url": "/nadav-avisrur-cv/static/css/main.ee76c508.chunk.css"
+  },
+  {
+    "revision": "87d0786ea1b2009e857e",
+    "url": "/nadav-avisrur-cv/static/js/2.b13e2519.chunk.js"
+  },
+  {
+    "revision": "7694ed5eff56d74dfc4b6ac3fc78a12b",
+    "url": "/nadav-avisrur-cv/static/js/2.b13e2519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19955586454447dd0cad",
+    "url": "/nadav-avisrur-cv/static/js/main.e7805fe0.chunk.js"
+  },
+  {
+    "revision": "9633b662d9dc7cabab08",
+    "url": "/nadav-avisrur-cv/static/js/runtime-main.edbca3c3.js"
+  },
+  {
+    "revision": "b097ec344272ccb0a033b4980312bebd",
+    "url": "/nadav-avisrur-cv/static/media/background.b097ec34.png"
+  },
+  {
+    "revision": "49a80a4e83165e7d8d74ce9d8eaa46cc",
+    "url": "/nadav-avisrur-cv/static/media/profile.49a80a4e.png"
+  }
+]);
